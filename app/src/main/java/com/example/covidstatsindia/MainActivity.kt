e7ef96255dde1ee476ddb497e165b6092285f34d
siddhraj rathod <skrathod282001@gmail.com>
2021-05-17 19:32:46 +0530

    adding search action in top app bar

--- app/src/main/java/com/example/covidstatsindia/MainActivity.kt
+++ app/src/main/java/com/example/covidstatsindia/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
@@ -12,10 +13,11 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.Locale.filter
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mAdapter: StatsAdapter
+  private lateinit var regionDataArray: ArrayList<CovidData>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,12 +27,57 @@ class MainActivity : AppCompatActivity() {
         progressbar.visibility = View.VISIBLE
         mAdapter = StatsAdapter()
         recyclerView.adapter = mAdapter
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.actionSearch -> {
+                    cardView.visibility = View.GONE
+                    searchState(it)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun searchState(it: MenuItem?) {
+         val searchView: SearchView = it?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+               return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filter(newText)
+                return false
+            }
+
+        })
+    }
+
+    private fun filter(newText: String?) {
+       val filteredList= ArrayList<CovidData>()
+        val i : CovidData
+       for(i in regionDataArray){
+           if(i.state.toLowerCase().contains(newText?.toLowerCase().toString())){
+               filteredList.add(i)
+           }
+       }
+        if (filteredList.isEmpty()) {
+            // if no item is added in filtered list we are
+            // displaying a toast message as no data found.
+            Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show();
+        } else {
+            // at last we are passing that filtered
+            // list to our adapter class.
+            mAdapter.filterList(filteredList);
+        }
     }
 
 
-
     private fun fetchData() {
-        val url = "https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true"
+        val url =
+            "https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true"
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET,
             url,
@@ -42,13 +89,13 @@ class MainActivity : AppCompatActivity() {
                 val overallRecoveredCases = it.getInt("recovered")
                 val overallDeath = it.getInt("deaths")
                 val overallTotalCases = it.getInt("totalCases")
-                tv_ovl_activeCases.text=overallActiveCases.toString()
-                tv_ovl_recovered_Cases.text=overallRecoveredCases.toString()
-                tv_ovl_death_cases.text=overallDeath.toString()
-                tv_ovl_total_cases.text=overallTotalCases.toString()
+                tv_ovl_activeCases.text = overallActiveCases.toString()
+                tv_ovl_recovered_Cases.text = overallRecoveredCases.toString()
+                tv_ovl_death_cases.text = overallDeath.toString()
+                tv_ovl_total_cases.text = overallTotalCases.toString()
                 //new code end
                 val regionDataJsonArray = it.getJSONArray("regionData")
-                val regionDataArray = ArrayList<CovidData>()
+                regionDataArray = ArrayList()
                 for (i in 0 until regionDataJsonArray.length()) {
                     val dataJsonObject = regionDataJsonArray.getJSONObject(i)
                     val data = CovidData(
